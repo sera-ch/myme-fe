7b64fcb22b35af2d36f1ff5431a5a2c5709da493
sera-ch <moonlightocgreview@gmail.com>
2025-04-25 16:37:03 +0700

    Fix config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,6 @@ export default defineConfig(({ mode }) => {
             'process.env.API_BASE_URL': JSON.stringify(env.API_BASE_URL)
         },
         plugins: [react()],
-        base: '/myme/'
+        base: '/myme-fe/'
  }
 })
